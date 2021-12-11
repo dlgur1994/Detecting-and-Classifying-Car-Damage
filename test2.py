77b73768c201b4e55ebf545e5214ef74420699a6
dlgur1994 <21400611@handong.edu>
2021-12-11 14:22:50 +0900

    updated 21/12/11

---
+++ test2.py
@@ -0,0 +1,110 @@
+# Importing Libraries
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import argparse
+
+from model import UNet
+from dataset import *
+from util import *
+
+
+# Parsing Inputs
+parser = argparse.ArgumentParser(description="Test the Model",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--lr", default=1e-3, type=float, dest="lr")
+parser.add_argument("--batch_size", default=4, type=int, dest="batch_size")
+
+parser.add_argument("--data_dir", default="./datasets_npy", type=str, dest="data_dir")
+parser.add_argument("--ckpt_dir", default="./checkpoint", type=str, dest="ckpt_dir")
+parser.add_argument("--result_dir", default="./result", type=str, dest="result_dir")
+
+args = parser.parse_args()
+
+
+# Setting Variables
+lr = args.lr
+batch_size = args.batch_size
+
+data_dir = args.data_dir
+ckpt_dir = args.ckpt_dir
+result_dir = args.result_dir
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+SMOOTH = 1e-6
+
+# Making Directories
+if not os.path.exists(result_dir):
+    os.makedirs(os.path.join(result_dir, 'jpg'))
+    os.makedirs(os.path.join(result_dir, 'numpy'))
+
+
+# Setting
+transform = transforms.Compose([Normalization(mean=0.5, std=0.5), ToTensor()])
+
+dataset_test = Dataset(img_dir=os.path.join(data_dir, 'test', 'images'), \
+                       label_dir=os.path.join(data_dir, 'test', 'masks'), \
+                       transform=transform)
+loader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=False, num_workers=8)
+
+num_data_test = len(dataset_test)
+
+num_batch_test = np.ceil(num_data_test / batch_size)
+
+
+# Creating Network
+from efficientunet import *
+net = get_efficientunet_b0(out_channels=1, concat_input=True, pretrained=True).to(device)
+
+
+# Defining Optimizer
+optim = torch.optim.Adam(net.parameters(), lr=lr)
+
+
+# Test Mode
+# defining functions for saving outputs
+fn_tonumpy = lambda x: x.to('cpu').detach().numpy().transpose(0, 2, 3, 1)
+fn_denorm = lambda x, mean, std: (x * std) + mean
+fn_class = lambda x: 1.0 * (x > -0.2) # dent: 0.1, scratch: -0.8, spacing: -1.1
+
+net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
+with torch.no_grad():
+    net.eval()
+    iou_scores = []
+
+    for batch, data in enumerate(loader_test, 1):
+        # forward pass
+        img = data['img'].to(device)
+        label = data['label'].to(device)
+        output = net(img)
+
+        # saving outputs
+        img = fn_tonumpy(fn_denorm(img, mean=0.5, std=0.5))
+        label = fn_tonumpy(label)
+        output = fn_tonumpy(fn_class(output))
+
+        # for j in range(label.shape[0]):
+        #     id = num_batch_test * (batch - 1) + j
+
+        #     plt.imsave(os.path.join(result_dir, 'jpg', 'image_%04d.png' % id), img[j].squeeze(), cmap='gray')
+        #     plt.imsave(os.path.join(result_dir, 'jpg', 'label_%04d.png' % id), label[j].squeeze(), cmap='gray')
+        #     plt.imsave(os.path.join(result_dir, 'jpg', 'output_%04d.png' % id), output[j].squeeze(), cmap='gray')
+
+        #     np.save(os.path.join(result_dir, 'numpy', 'image_%04d.npy' % id), img[j].squeeze())
+        #     np.save(os.path.join(result_dir, 'numpy', 'label_%04d.npy' % id), label[j].squeeze())
+        #     np.save(os.path.join(result_dir, 'numpy', 'output_%04d.npy' % id), output[j].squeeze())
+
+        label_temp = label.squeeze(3).astype(int)
+        output_temp = output.squeeze(3).astype(int)
+
+        intersection = np.sum(label_temp & output_temp)
+        union = np.sum(label_temp | output_temp)
+        iou_score = (intersection + SMOOTH) / (union + SMOOTH)
+
+        iou_scores.append(iou_score)
+        print("TEST: BATCH %04d \ %04d | IoU %.4f" % (batch, num_batch_test, iou_score))
+
+print("AVERAGE TEST: BATCH %04d \ %04d | mIoU %.4f" % (batch, num_batch_test, sum(iou_scores)/len(iou_scores)))
---
+++ train2.py
@@ -0,0 +1,134 @@
+# Importing Libraries
+import matplotlib.pyplot as plt
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import argparse
+
+import wandb
+wandb.init(project="U-Net", entity="kim1lee3")
+
+from efficientunet import *
+from dataset import *
+from util import *
+
+
+# Parsing Inputs
+parser = argparse.ArgumentParser(description="Train the Model",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument("--lr", default=1e-3, type=float, dest="lr")
+parser.add_argument("--batch_size", default=4, type=int, dest="batch_size")
+parser.add_argument("--num_epoch", default=100, type=int, dest="num_epoch")
+
+parser.add_argument("--data_dir", default="./datasets_npy", type=str, dest="data_dir")
+parser.add_argument("--ckpt_dir", default="./checkpoint", type=str, dest="ckpt_dir")
+
+parser.add_argument("--train_continue", default="off", type=str, dest="train_continue")
+
+args = parser.parse_args()
+
+
+# Setting Variables
+lr = args.lr
+batch_size = args.batch_size
+num_epoch = args.num_epoch
+
+data_dir = args.data_dir
+ckpt_dir = args.ckpt_dir
+
+train_continue = args.train_continue
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+wandb.config = {
+  "learning_rate": args.lr,
+  "epochs": args.num_epoch,
+  "batch_size": args.batch_size
+}
+
+
+# Network
+transform = transforms.Compose([Normalization(mean=0.5, std=0.5), RandomFlip(), ToTensor()])
+
+dataset_train = Dataset(img_dir=os.path.join(data_dir, 'train', 'images'), \
+                              label_dir=os.path.join(data_dir, 'train', 'masks'), \
+                              transform=transform)
+loader_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, num_workers=8)
+
+dataset_val = Dataset(img_dir=os.path.join(data_dir, 'valid', 'images'), \
+                            label_dir=os.path.join(data_dir, 'valid', 'masks'), \
+                            transform=transform)
+loader_val = DataLoader(dataset_val, batch_size=batch_size, shuffle=False, num_workers=8)
+
+num_data_train = len(dataset_train)
+num_data_val = len(dataset_val)
+
+num_batch_train = np.ceil(num_data_train / batch_size)
+num_batch_val = np.ceil(num_data_val / batch_size)
+
+
+# Creating Network
+# net = get_efficientunet_b0(out_channels=1, concat_input=True, pretrained=True).to(device)
+# net = get_efficientunet_b1(out_channels=1, concat_input=True, pretrained=True).to(device)
+# net = get_efficientunet_b2(out_channels=1, concat_input=True, pretrained=True).to(device)
+net = get_efficientunet_b3(out_channels=1, concat_input=True, pretrained=True).to(device)
+
+
+# Defining Loss Function
+fn_loss = nn.BCEWithLogitsLoss().to(device)
+
+
+# Defining Optimizer
+optim = torch.optim.Adam(net.parameters(), lr=lr)
+
+
+# Train
+st_epoch = 0
+
+if train_continue == "on":
+    net, optim, st_epoch = load(ckpt_dir=ckpt_dir, net=net, optim=optim)
+
+for epoch in range(st_epoch + 1, num_epoch + 1):
+    net.train()
+    loss_train = []
+
+    for batch, data in enumerate(loader_train, 1):
+        # forward pass
+        img = data['img'].to(device)
+        label = data['label'].to(device)
+        output = net(img)
+
+        # backward pass
+        optim.zero_grad()
+        loss = fn_loss(output, label)
+        loss.backward()
+        optim.step()
+
+        # calculate loss function
+        loss_train += [loss.item()]
+        print("Train: EPOCH %04d / %04d | BATCH %04d \ %04d | LOSS %.4f"
+              % (epoch, num_epoch, batch, num_batch_train, np.mean(loss_train)))
+
+    wandb.log({"train loss": np.mean(loss_train)})
+
+    with torch.no_grad():
+        net.eval()
+        loss_valid = []
+
+        for batch, data in enumerate(loader_val, 1):
+            # forward pass
+            img = data['img'].to(device)
+            label = data['label'].to(device)
+            output = net(img)
+
+            # calculate loss function
+            loss = fn_loss(output, label)
+            loss_valid += [loss.item()]
+            print("Validation: EPOCH %04d / %04d | BATCH %04d \ %04d | LOSS %.4f"
+                  % (epoch, num_epoch, batch, num_batch_val, np.mean(loss_valid)))
+
+    wandb.log({"valid loss": np.mean(loss_valid)})
+
+    if epoch % 10 == 0:
+        save(ckpt_dir=ckpt_dir, net=net, optim=optim, epoch=epoch)
